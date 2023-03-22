2ab799a7870900dbfcbbfd558c1fb143bccf69c2
SalvatoreBrancato <salvatorebrancato90@gmail.com>
2023-03-22 18:58:15 +0100

    Struttura_definitiva

--- assets.js/main.js
+++ assets.js/main.js
@@ -1,9 +1,7 @@
-
-// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
-    // prezzo in € con due decimali
-    
     let km = prompt("Inserisci il numero di chilometri che vuoi percorrere");
     let età = prompt("Inserisci l' età del passeggero");
+    let importoKm = "0.21";
+
     let sconto;
     if (età <18) {
         sconto = "20%";
@@ -15,22 +13,24 @@
     else if (età > 60){
         sconto = "40%";
     }
-
+    
     let prezzo;
     if (età <18){
-        prezzo = (km * 0.21 * 0.8);
+        prezzo = (km * importoKm * 0.8);
     }
     else if (età >=18 && età <=65){
-        prezzo = (km * 0.21);
+        prezzo = (km * importoKm);
     }
     else if (età >65){
-        prezzo = (km * 0.21 * 0.6);
+        prezzo = (km * importoKm * 0.6);
     }
+    
+    prezzo = prezzo.toFixed(2);
 
     console.log(prezzo);
     document.getElementById(`km`).innerHTML = `La distanza selezionata è di: ${km} km `;
     document.getElementById(`eta`).innerHTML = `L'età selezionata è di anni: ${età} `;
-    document.getElementById(`prezzo`).innerHTML = `Il prezzo del biglietto è di €: ${km * 0.21} `;
+    document.getElementById(`prezzo`).innerHTML = `Il prezzo del biglietto è di €: ${km * importoKm} `;
     document.getElementById(`sconto`).innerHTML = `Sconto applicato: ${sconto} `; 
     document.getElementById(`totale`).innerHTML = `L'importo totale è di € ${prezzo} `;  
 
